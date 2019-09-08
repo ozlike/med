3f878c52bb00a3b0b5deb69a920f448566847922
Alexander <97belov@gmail.com>
2019-09-08 15:33:52 +0300

    Date fix

--- TestTask/wwwroot/js/dateControl.js
+++ TestTask/wwwroot/js/dateControl.js
@@ -1,19 +1,37 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
+﻿let dateSpan, datepicker, formDateInput;
+
+document.addEventListener("DOMContentLoaded", function () {
+    dateSpan = document.getElementById("dateSpan");
+    datepicker = document.getElementById("datepicker");
+    formDateInput = document.getElementById("formDate");
+
+    datepicker.addEventListener("input", checkDate);
+    
     document.getElementById("submitBtn").addEventListener("click", function (e) {
-        let date = new Date(document.getElementById("datepicker").value);
-        let dateSpan = document.getElementById("dateSpan");
-        if (isNaN(date) || date > new Date() || new Date().getFullYear() - date.getFullYear() > 150) {
-            dateSpan.innerHTML = "Неверная дата";
-            e.preventDefault();
-        }
-        else dateSpan.innerHTML = "";
+        if (!checkDate()) e.preventDefault();
     });
 });
+
+function checkDate() {
+    let dateArray = datepicker.value.split('.').filter(x=>x.length > 0);
+    if (dateArray.length != 3) return showInvalidDateMessage();
+    var date = new Date(dateArray[2], parseInt(dateArray[1]) - 1, dateArray[0]);
+    if (isNaN(date) || date > new Date() || new Date().getFullYear() - date.getFullYear() > 150) return showInvalidDateMessage();
+    formDateInput.value = dateArray[2] + "-" + dateArray[1] + "-" + dateArray[0];
+    dateSpan.innerHTML = "";
+    return true;
+}
+
+function showInvalidDateMessage() {
+    dateSpan.innerHTML = "Неверная дата";
+    return false;
+}
 
 $(function () {
     $("#datepicker").datepicker({
         minDate: '-150Y',
         maxDate: 'today',
-        dateFormat: 'dd.mm.yy'
+        dateFormat: 'dd.mm.yy',
+        onSelect: checkDate
     });
 });
